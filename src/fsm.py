503c161c8d4beec6cf221413d47d4b0f9e078172
gruj1995 <fangyu070899@gmail.com>
2022-12-25 02:17:47 +0800

    add fsm

--- src/fsm.py
+++ src/fsm.py
@@ -28,55 +28,8 @@ class TocMachine(GraphMachine):
     def is_going_to_disease_category2(self, event):
         print("go to disease_category2")
     
-    def is_going_to_bone_disease(self, event):
+    def is_going_to_disease(self, event):
         print("go to bone_disease")
     
-    def is_going_to_cranial_disease(self, event):
-        print("go to cranial_disease")
-    
-    def is_going_to_ear_disease(self, event):
-        print("go to ear_disease")
-    
-    def is_going_to_eye_disease(self, event):
-        print("go to eye_disease")
-    
-    def is_going_to_gynecology_disease(self, event):
-        print("go to gynecology_disease")
-    
-    def is_going_to_kidney_disease(self, event):
-        print("go to kidney_disease")
-    
-    def is_going_to_liver_disease(self, event):
-        print("go to liver_disease")
-    
-    def is_going_to_lung_disease(self, event):
-        print("go to lung_disease")
-    
-    def is_going_to_mind_disease(self, event):
-        print("go to mind_disease")
-    
-    def is_going_to_skin_disease(self, event):
-        print("go to skin_disease")
-    
-    def is_going_to_heart_disease(self, event):
-        print("go to heart_disease")
-    
-    def is_going_to_stomach_disease(self, event):
-        print("go to stomach_disease")
-    
-    def is_going_to_urinary_disease(self, event):
-        print("go to urinary_disease")
-    
-    def is_going_to_male_symptom(self, event):
+    def is_going_to_symptom(self, event):
         print("go to male_symptom")
-    
-    def is_going_to_female_symptom(self, event):
-        print("go to female_symptom")
-    
-    def is_going_to_elder_symptom(self, event):
-        print("go to elder_symptom")
-    
-    def is_going_to_child_symptom(self, event):
-        print("go to child_symptom")
-    
-    
